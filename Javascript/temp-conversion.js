cc4eb6afd16095a869a64e7fe3215a2154a26e69
ItsProgramCat <38403087+ItsProgramCat@users.noreply.github.com>
2018-05-04 22:15:02 -0400

    Add files via upload

---
+++ Javascript/temp-conversion.js
@@ -0,0 +1,13 @@
+// Create a variable for fahrenheit
+let fahrenheit = 50
+
+// calculate celsisua nd store in celsius variable
+
+let celsius = (fahrenheit - 32) * (5/9) + 'c'
+
+let kelvin = (fahrenheit + 459.67) * 5/9 + 'k'
+// print value
+console.log(celsius)
+console.log(kelvin)
+
+
