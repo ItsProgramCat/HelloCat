85ea43d926e7c1fb254b38cf2f82a7307f00ead0
ItsProgramCat <38403087+ItsProgramCat@users.noreply.github.com>
2018-05-04 22:15:43 -0400

    Update temp-conversion.js

--- Javascript/temp-conversion.js
+++ Javascript/temp-conversion.js
@@ -1,7 +1,7 @@
 // Create a variable for fahrenheit
 let fahrenheit = 50
 
-// calculate celsisua nd store in celsius variable
+// calculate celsisus and store in celsius variable
 
 let celsius = (fahrenheit - 32) * (5/9) + 'c'
 
